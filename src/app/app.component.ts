2eea8b145e0f210db60ab28043b5e0f5599352f3
unknown <allblue312@gmail.com>
2020-10-19 18:44:42 +0300

    scroll ne rabotaet(

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'demo';
+
+  @ViewChild('main') main: ElementRef;
+
+  @ViewChild('aboutPugs') aboutPugs: ElementRef;
+
+  @ViewChild('historyPugs') historyPugs: ElementRef;
+
+  public previousScroll = 0;
+
+  @HostListener('scroll', ['$event']) onScroll($event:Event) {
+    console.log($event);
+    // if (this.previousScroll < ($event.target as HTMLElement).scrollTop) {
+    //   console.log('1');
+    // }
+    // else{
+    //   console.log('2');
+    // }
+    // this.previousScroll = ($event.target as HTMLElement).scrollTop;
+    };
+
+  scrollToMain() {
+    this.main.nativeElement.scrollIntoView({ behavior: 'smooth'})
+  }
+
+  scrollToSecondPage() {
+    this.aboutPugs.nativeElement.scrollIntoView({ behavior: 'smooth'})
+  }
+  
+  scrollToThirdPage() {
+    this.historyPugs.nativeElement.scrollIntoView({ behavior: 'smooth'})
+  }
+  
 }
